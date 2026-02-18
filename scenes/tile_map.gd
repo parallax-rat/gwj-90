4e076ee8cdfd39049022ece95636d4f8d8d55737
siegel <reach.me.et+github@gmail.com>
2026-02-18 22:37:15 +0200

    attempt

--- scenes/tile_map.gd
+++ scenes/tile_map.gd
@@ -3,8 +3,9 @@ extends TileMapLayer
 
 signal clicked(coords)
 
-func _input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("select"):
+		print('unhandled input')
 		var clicked_cell = local_to_map(get_local_mouse_position())
 		var half_tile = Global.HEX_SIZE / 2
 		var coords = map_to_local(clicked_cell) #- Vector2(half_tile,half_tile)
